4fdd10adc069fd01d42119bdc12510347fae5379
单小飞 <shanpengfei@duolaimei.net>
2019-04-11 14:06:16 +0800

    Initial Commit

---
+++ CodeSpace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeSpace
+//
+//  Created by spf on 2019/4/11.
+//  Copyright © 2019 spf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeSpace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeSpace
+//
+//  Created by spf on 2019/4/11.
+//  Copyright © 2019 spf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
